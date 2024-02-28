8ff4acc3c08b17295e276d1030ae09399ada00f6
Krish Saluja <krishsaluja297@gmail.com>
2024-02-28 15:17:10 +0530

    working on allowing the user to input the recipe and get results.

---
+++ index.js
@@ -0,0 +1,8 @@
+btn.addEventListener("click",()=>{
+    const inputElement = document.getElementById(searchR);
+    searchR = inputElement.value;
+async function logRecipes(){
+    const response = await fetch(`www.themealdb.com/api/json/v1/1/search.php?s=${searchR}`)
+    const recipes = await response.json();
+    console.log(recipes)
+}});
