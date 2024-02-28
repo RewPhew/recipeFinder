1b542331379d98db977261f07292e987671b5ab6
RewPhew <160760699+RewPhew@users.noreply.github.com>
2024-02-28 15:50:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 btn.addEventListener("click",()=>{
-    const inputElement = document.getElementById(searchR);
+    const inputElement = document.getElementById('searchR');
     searchR = inputElement.value;
 async function logRecipes(){
     const response = await fetch(`www.themealdb.com/api/json/v1/1/search.php?s=${searchR}`)
